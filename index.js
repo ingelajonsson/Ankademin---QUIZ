83a446f694f8be1c1ea0057565abc2a6579e4fb2
Ingela Jonsson <ingela.ma.jonsson@gmail.com>
2022-10-25 13:43:29 +0200

    Shortening of code etc.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let submitBtn = document.querySelector("#submitBtn");
-let result = document.querySelector("#result");
 let themeBtn = document.querySelector("#themeBtn");
 let robotCheck = document.querySelector("#robotCheck");
+let submitBtn = document.querySelector("#submitBtn");
+let result = document.querySelector("#result");
 
 themeBtn.addEventListener("click", () => {
     let theme = document.getElementsByTagName("link")[0];
@@ -23,8 +23,6 @@ function isRobot(){
 
 submitBtn.addEventListener("click", () => {
     if(isRobot()) {
-        result.innerText = "";
-        
         let name = document.querySelector("#name");
         let radioQuestions = document.querySelectorAll("[type='radio']:checked");
         let arrAnswers = [];
@@ -35,48 +33,36 @@ submitBtn.addEventListener("click", () => {
             }
         })
 
-        let checkArr1 = []; 
         let q3 = document.querySelectorAll("[name='q3']:checked");
-        q3.forEach(item => {
-            if(item.value !== "wrong"){
-                checkArr1.push(item.value);
-                if(checkArr1.length === 3 && q3.length === 3){
-                arrAnswers.push("correct");
-                }
-            }
-        })
-
-        let checkArr2 = []; 
         let q7 = document.querySelectorAll("[name='q7']:checked");
-        q7.forEach(item => {
-            if(item.value !== "wrong"){
-                checkArr2.push(item.value);
-                if(checkArr2.length === 2 && q7.length === 2){
-                arrAnswers.push("correct");
+        let checkArrQ3 = []; 
+        let checkArrQ7 = []; 
+
+        function checkboxes (x, y){
+            x.forEach(item => {
+                if(item.value === "correct"){
+                    y.push(item.value);
+                    if(y.length === 2 && x.length === 2){
+                    return arrAnswers.push("correct");
+                    }
                 }
-            }
-        })
+            })
+        }
+
+        checkboxes(q3, checkArrQ3);
+        checkboxes(q7, checkArrQ7);
 
         if(arrAnswers.length < 5){
-        let score = document.createElement("h3");
-        score.innerText = `Better luck next time ${name.value}, you have failed. You're score is ${arrAnswers.length}/10`
-        score.style.color = "red";
-
-        result.append(score);
+        result.innerText = `Better luck next time ${name.value}, you have failed. You're score is ${arrAnswers.length}/10`
+        result.style.color = "red";
         }
         else if(arrAnswers.length >= 5 && arrAnswers.length < 8){
-        let score = document.createElement("h3");
-        score.innerText = `Well done ${name.value}, you have passed the test. You're score is ${arrAnswers.length}/10`
-        score.style.color = "orange";
-
-        result.append(score);
+        result.innerText = `Well done ${name.value}, you have passed the test. You're score is ${arrAnswers.length}/10`
+        result.style.color = "orange";
         } 
         else if(arrAnswers.length >= 8){
-        let score = document.createElement("h3");
-        score.innerText = `Amazing ${name.value}, you passed the test with flying colors! You're score is ${arrAnswers.length}/10`
-        score.style.color = "green";
-
-        result.append(score);
+        result.innerText = `Amazing ${name.value}, you passed the test with flying colors! You're score is ${arrAnswers.length}/10`
+        result.style.color = "green";
         } 
 
         let key = document.querySelectorAll("[value]");
